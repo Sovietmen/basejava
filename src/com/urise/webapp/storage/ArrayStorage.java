9b69c750e4e0354785a6a29d64061658e3c0642d
Nikolay Bernyakovich <n.bernyakovich@gmail.com>
2021-01-19 21:38:46 +0300

    Add abstract method saveMethod() for difference method of saving resume.

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,12 +7,20 @@ import com.urise.webapp.model.Resume;
  */
 public class ArrayStorage extends AbstractArrayStorage {
 
-	protected int findIndex(String uuid) {
-		for (int i = 0; i < size; i++) {
-			if (uuid.equals(storage[i].getUuid())) {
-				return i;
-			}
-		}
-		return -1;
-	}
+    @Override
+    protected int findIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    protected void saveMethod(Resume resume) {
+        int indexOfResume = size;
+        storage[indexOfResume] = resume;
+        size++;
+    }
 }
--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -6,10 +6,19 @@ import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
 
-	@Override
-	protected int findIndex(String uuid) {
-		Resume searchKey = new Resume();
-		searchKey.setUuid(uuid);
-		return Arrays.binarySearch(storage, 0, size, searchKey);
-	}
+    @Override
+    protected void saveMethod(Resume resume) {
+        int indexOfResume = findIndex(resume.getUuid());
+        indexOfResume = -(indexOfResume) - 1;
+        System.arraycopy(storage, indexOfResume, storage, indexOfResume + 1, size - indexOfResume);
+        storage[indexOfResume] = resume;
+        size++;
+    }
+
+    @Override
+    protected int findIndex(String uuid) {
+        Resume searchKey = new Resume();
+        searchKey.setUuid(uuid);
+        return Arrays.binarySearch(storage, 0, size, searchKey);
+    }
 }
